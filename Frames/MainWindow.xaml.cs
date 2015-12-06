614aefeed75bc0ca1a184e4e1a3b4744c4e23007
mcdudr01 <mcdudr01@gmail.com>
2015-12-06 12:32:10 -0500

    Polished UI

---
+++ Frames/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DSAnimator9000
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Menu_New_Click(object sender, RoutedEventArgs e)
+        {
+            txt_code.Text = "";
+        }
+
+        private void Menu_Open_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "CSharp files (*.cs)|*.cs|Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+                txt_code.Text = File.ReadAllText(openFileDialog.FileName);
+        }
+
+        private void Menu_Save_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "CSharp file (*.cs)|*.cs|Text file (*.txt)|*.txt|C# file (*.cs)|*.cs";
+            if (saveFileDialog.ShowDialog() == true)
+                File.WriteAllText(saveFileDialog.FileName, txt_code.Text);
+        }
+
+        private void Menu_Exit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Menu_Animate_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_back_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_stop_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_start_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_forward_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        //private void txtWindow_Loaded(object sender, RoutedEventArgs e)
+        //{
+
+        //}
+    }
+}
